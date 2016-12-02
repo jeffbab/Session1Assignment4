80dfe8a70a718836dd3cd080b2a0ce02a0fe6ed3
jeffbab <jefferybabineaux@gmail.com>
2016-12-02 10:32:32 +0430

    Updates

---
+++ Session1,4/src/JavaSession1Assignment4.java
@@ -0,0 +1,21 @@
+
+public class JavaSession1Assignment4 {
+
+	/*
+	 * Assume that the integer variable x is 5 and the integer variable y is 10. Give the values of the following
+	 * expressions:
+	 * a. x + y * 2 = 25
+	 * b. x - y + 2 = -3
+	 * c. (x+y) * 2 = 30
+	 * d. y % x = 0
+	 */
+	public static void main(String[] args) {
+
+		int x = 5;
+		int y = 10;
+		
+		System.out.println((x+y*2)+"\n"+(x-y+2)+"\n"+((x+y)*2)+"\n"+y%x);
+
+	}
+
+}
